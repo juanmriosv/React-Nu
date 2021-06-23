efa79e48a025d28aa52c1d26e528efa310593ec1
Juan Manuel Rios Villanueva <juanma@theatico.com>
2021-06-22 21:54:23 -0400

    w3 first commit

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
+    renderComments(comments){
+        if (comments){
+            return(
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(c => (<div>
+                        <p>
+                            <span className="jmr-comment">{c.text}</span>
+                            --{c.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(c.date)))}
+                        </p>
+                    </div>))}
+                </div>
+            );
+        }
+        return <div />;
+
+    }
     renderCampsite(campsite){
         return (
             <div className="col-md-5 m-1">
@@ -13,11 +30,14 @@ class CampsiteInfo extends Component {
                     </CardBody>
                 </Card>
             </div>
-        )
+        );
     }
     render(){
         if (this.props.campsite){
-            return <div className="row">{this.renderCampsite(this.props.campsite)}</div>
+            return <div className="row">
+                {this.renderCampsite(this.props.campsite)}
+                {this.renderComments(this.props.campsite.comments)}
+            </div>
         }
         return <div />
     }
