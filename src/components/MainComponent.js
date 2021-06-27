cd438960abf1baaf48e75e62f54d5e33b50e499a
Juan Manuel Rios Villanueva <juanma@theatico.com>
2021-06-27 01:34:56 -0400

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent'
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 import Header from './HeaderComponent';
 import Home from './HomeComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Footer from './FooterComponent';
 
@@ -12,6 +17,9 @@ class Main extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -19,7 +27,26 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            );
+        };
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        };
+
+        const ContactUs = () => {
+            return (
+                <Contact />
             );
         };
 
@@ -28,7 +55,9 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
+                    <Route exact path='/contactus' component={ContactUs} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
