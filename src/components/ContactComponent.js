7f34aec2ce53a863d17e1f570f20a99ec1526632
Juan Manuel Rios Villanueva <juanma@theatico.com>
2021-07-02 01:34:28 -0400

    end before workshop w4

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Button, Label, Col, Row } from 'reactstrap';
-import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
 const required = val => val && val.length;
