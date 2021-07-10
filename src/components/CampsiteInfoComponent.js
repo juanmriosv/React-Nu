2ab9d50c6e53369fff1fccc9c41aaf6c0adbad3f
Juan Manuel Rios Villanueva <juanma@theatico.com>
2021-07-10 00:14:28 -0400

    fetch server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Button, Label, Modal, ModalHeader, ModalBody, Card, CardImg, CardText, CardTitle, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -127,7 +128,7 @@ function RenderCampsite({campsite}){
     return (
         <div className="col-md-5 m-1">
             <Card>
-                <CardImg src={campsite.image} alt={campsite.name} />
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardTitle>{campsite.name}</CardTitle>
                     <CardText>{campsite.description}</CardText>
